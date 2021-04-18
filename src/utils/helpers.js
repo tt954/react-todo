92bda0dd3a64f33945068a2ce7912ecaee8023d8
Tieulam Thai <ttl.t954@gmail.com>
2021-04-17 22:42:58 -0600

    add: capitalize helper fnc

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,6 @@
 export const generateRandomColor = () => {
-    const colors = ["Red", "Yellow", "Green", "Blue", "Orange", "Purple", null];
+    const colors = ["Red", "Yellow", "Green", "Blue", "Orange", "Purple"];
     return colors[[Math.floor(Math.random() * colors.length)]];
 }
+
+export const capitalize = s => s[0].toUpperCase + s.slice(1) 
